3eb6a20568673ac63ad6571d412f9f58c613c0a4
Thiago Barros <tjabarros@hotmail.com>
2019-08-25 02:17:54 -0300

    add category and product

---
+++ database/migrations/1566710049580_product_schema.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ProductSchema extends Schema {
+  up () {
+    this.create('products', (table) => {
+      table.increments()
+      table.string('name', 200)
+      table.text('description')
+      table.decimal('price', 12, 2)
+      table.integer('image_id')
+      table.foreign('image_id').references('id').inTable('images').onDelete('cascade')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('products')
+  }
+}
+
+module.exports = ProductSchema
